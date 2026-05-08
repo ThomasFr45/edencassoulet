fb38fb74eb73261b35510e15f0eded4265bb3a03
ThomasFr45 <thomasfran1935@gmail.com>
2026-05-08 08:22:41 -0400

    title and infos on tk page

--- src/components/wiki/Tk.jsx
+++ src/components/wiki/Tk.jsx
@@ -3,7 +3,16 @@ import "./tk.css"
 const Tk = () => {
     return (
         <div className="tkBody">
-            <h2 className="tkTitle">- Temple Knight Trial -</h2>
+            <h1 className="tkTitle">~ Temple Knight Trial ~</h1>
+            <div className="tkInfos">
+                <img src="https://i.imgur.com/WWxfDNh.png" alt="Tyrosen" className="tkInfosImage"/>
+                <div className="tkInfosText">
+                    <p>NPC Name : Tyrosen (x:325, yx275).</p>
+                    <p>Lv: 80</p>
+                    <p>5 players</p>
+                    <p>Title : +10% all stats</p>
+                </div>
+            </div>
         </div>
     );
 }
