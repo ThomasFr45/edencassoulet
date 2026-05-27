0327571fa6f602ba8e7a88b7547a6cb8540c439a
ThomasFr45 <thomasfran1935@gmail.com>
2026-05-27 09:01:04 -0400

    title + basic infos on awa

--- src/components/wiki/awaClass/AwaClass.jsx
+++ src/components/wiki/awaClass/AwaClass.jsx
@@ -1,6 +1,18 @@
+import './awaClass.css';
+import { useContext } from 'react';
+import MyContext from '../../../context/MyContext';
+import awaClassGeneral from '../../../content/awaClassGeneral';
+
 const AwaClass = () => {
+    const [ awaClassGenInfos ] = awaClassGeneral;
+    const { language } = useContext(MyContext);
     return (
-        <div>PROUT</div>
+        <div className='awaClassContainer'>
+            <h1 className='awaClassTitle'>{language === 'fr' ? 'Classes Éveillées' : 'Awaken Classes'}</h1>
+            <div className='awaClassGeneral'>
+                {language === 'fr' ? awaClassGenInfos.fr.split('\n').map(line => <p>{line}</p>) : 'Not translated yet'}
+            </div>
+        </div>
     )
 };
 
---
+++ src/content/awaClassGeneral.js
@@ -0,0 +1,8 @@
+const awaClassGeneral = [
+    {
+        fr:"Pour éveiller votre classe, il faudra remplir quelques critères. Premièrement il faudra que vous soyez niveau 85, ensuite que votre classe soit niveau 80, avoir terminé la quête 'Lv83. Legacy of the Eternal Heroes', et enfin il faudra acheter et compléter les quêtes d'éveil de votre classe. \n  Ces quêtes sont relativement difficile en fonction de votre stuff actuel, et vous demanderont pas mal d'investissement, il est recommandé de vous y attaquer une fois que vous avez un équipement solide. \n Notez qu'il faudra prévoir entre 25.000 et 30.000 gold en fonction de la classe que vous voulez éveiller.",
+        en:"",
+    },
+];
+
+export default awaClassGeneral;
