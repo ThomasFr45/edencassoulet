ea8dd753602154ebf566ebac649131d457afc3d1
ThomasFr45 <thomasfran1935@gmail.com>
2026-05-31 19:28:46 -0400

    CSS and done with day selection / display

--- src/components/wiki/dDoor/Ddoor.jsx
+++ src/components/wiki/dDoor/Ddoor.jsx
@@ -1,8 +1,10 @@
 import dDoor from '../../../content/dDoor';
 import './dDoor.css';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import myContext from '../../../context/MyContext';
 
 const Ddoor = () => {
+    const { language } = useContext(myContext);
     const localTime = new Date();
     const serverTimeZone = "Canada/Eastern"
     const serverTime = new Intl.DateTimeFormat('en-US', {
@@ -11,8 +13,8 @@ const Ddoor = () => {
         timeStyle: 'full',
     }).format(localTime)
     const day = serverTime.split(',')[0];
-    const [ selected, setSelected ] = useState(day)
-    const handleDaySelection = (clickedDay) => {
+    const [ selected, setSelected ] = useState(day);
+    const handleDaySelection = (clickedDay, dayId) => {
         setSelected(clickedDay);
         return;
     };
@@ -20,26 +22,30 @@ const Ddoor = () => {
         <div>
             <div className='weekContainer'>
                 <div className={selected === 'Monday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Monday")
+                    handleDaySelection("Monday",0)
                 }}>Monday</div>
                 <div className={selected === 'Tuesday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Tuesday")
+                    handleDaySelection("Tuesday",1)
                 }}>Tuesday</div>
                 <div className={selected === 'Wednesday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Wednesday")
+                    handleDaySelection("Wednesday",2)
                 }}>Wednesday</div>
                 <div className={selected === 'Thursday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Thursday")
+                    handleDaySelection("Thursday",3)
                 }}>Thursday</div>
                 <div className={selected === 'Friday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Friday")
+                    handleDaySelection("Friday",4)
                 }}>Friday</div>
                 <div className={selected === 'Saturday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Saturday")
+                    handleDaySelection("Saturday",5)
                 }}>Saturday</div>
                 <div className={selected === 'Sunday' ? 'weekDaySelected' : 'weekDay'} onClick={() => {
-                    handleDaySelection("Sunday")
+                    handleDaySelection("Sunday",6)
                 }}>Sunday</div>
+            </div>
+            <div className='dayInfos'>
+                <p className='dayFirstInfo'>DoubleDoor (Lv70): {language === 'en' ? dDoor[0][selected].split('\n')[0] : dDoor[0][selected].split('\n')[1] ? dDoor[0][selected].split('\n')[1] : dDoor[0][selected].split('\n')[0]}</p>
+                <p>Ma'Kai (Lv80): {language === 'en' ? dDoor[1][selected].split('\n')[0] : dDoor[1][selected].split('\n')[1]  ? dDoor[1][selected].split('\n')[1] : dDoor[1][selected].split('\n')[0]}</p>
             </div>
         </div>
     )
